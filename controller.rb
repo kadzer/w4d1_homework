a63398325cdec7bfd67c0dc413466346945f495b
Kaspars Dzerins <kadzer@gmail.com>
2018-08-07 00:02:14 +0100

    complete, not sure if this covers further ext requirements. not happy about method

--- controller.rb
+++ controller.rb
@@ -6,9 +6,10 @@ require_relative( './models/game' )
 also_reload( './models/*' )
 
 get '/play/:player1/:player2' do
+  game = Game.new(params[:player1].to_s.downcase, params[:player2].to_s.downcase)
+  @outcome = game.compare_choices()
   erb(:result)
 end
-
 
 get '/welcome' do
   erb(:welcome)
--- models/game.rb
+++ models/game.rb
@@ -1,8 +1,39 @@
 class Game
+  attr_reader :player1, :player2
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
   end
 
+  def compare_choices()
+# there has to be a more elegant way of doing this, maybe iterating through the array somehow?
+    if @player1 == @player2
+      return "It's a draw: play again!"
+    end
 
+    if @player1 == "rock"
+      if@player2 == "scissors"
+        return "Rock smashes scissors: Player1 wins!"
+      elsif @player2 == "paper"
+        return "Paper covers rock: Player2 wins!"
+      end
+    end
+
+    if @player1 == "paper"
+      if@player2 == "scissors"
+        return "Scissors cut paper: Player2 wins!"
+      elsif @player2 == "rock"
+        return "Paper covers rock: Player1 wins!"
+      end
+    end
+
+    if @player1 == "scissors"
+      if@player2 == "paper"
+        return "Scissors cut paper: Player1 wins!"
+      elsif @player2 == "rock"
+        return "Rock smashes scissors: Player2 wins!!"
+      end
+    end
+
+  end
 end
